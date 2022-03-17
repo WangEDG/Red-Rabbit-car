8cec19a28e5d1acc59e4e832988fd4a12e52459d
WangShiLin <185679354@qq.com>
2022-03-17 22:01:10 +0800

    暂存test分支代码

--- src/utils/verification.js
+++ src/utils/verification.js
@@ -1,9 +1,9 @@
 //空对象判断
 export const Objempty = function (obj) {
-  if (Object.keys(obj).length === 0) {
+  if (Object.keys(obj).length >= 0) {
     return true;
   } else {
-    return false;
+    return 123;
   }
 }
 
