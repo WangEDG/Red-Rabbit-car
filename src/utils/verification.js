88868d63f9c29f94eb44bdecf5989ee7e6946290
WangShiLin <185679354@qq.com>
2022-03-17 22:05:08 +0800

    复原代码

--- src/utils/verification.js
+++ src/utils/verification.js
@@ -1,9 +1,9 @@
 //空对象判断
 export const Objempty = function (obj) {
-  if (Object.keys(obj).length >= 0) {
+  if (Object.keys(obj).length === 0) {
     return true;
   } else {
-    return 123;
+    return false;
   }
 }
 
