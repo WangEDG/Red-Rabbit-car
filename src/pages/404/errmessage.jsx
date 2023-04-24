9dc2dd48da345daecbf1a3d064e54ef8940a4823
卡布达 <185679354@qq.com>
2023-04-24 22:32:35 +0800

    feat:分支修改 制造冲突

--- src/pages/404/errmessage.jsx
+++ src/pages/404/errmessage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function errmessage() {
   return (
     <div>
-      {/* <h1>页面走丢了..........</h1> */}
+      {/* <h1>页面走丢了.....页面走丢了啊.....</h1> */}
       <h1>页面走丢了..........</h1>
     </div>
   );
